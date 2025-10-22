d49985f1997d3ef9ed0fd7364b554c6ebe85085f
Olaf Monien <olaf@monien.net>
2025-10-22 15:36:50 +0200

    initial

---
+++ DxPdfViewer.dpr
@@ -0,0 +1,14 @@
+﻿program DxPdfViewer;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  Main.Form in 'Main.Form.pas' {Form48};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm48, Form48);
+  Application.Run;
+end.
---
+++ Main.Form.pas
@@ -0,0 +1,24 @@
+﻿unit Main.Form;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+
+type
+  TForm48 = class(TForm)
+  private
+    { Private-Deklarationen }
+  public
+    { Public-Deklarationen }
+  end;
+
+var
+  Form48: TForm48;
+
+implementation
+
+{$R *.fmx}
+
+end.
